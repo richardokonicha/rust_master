aecbe28c499db7422c303ad17e0c9a1fc7187716
riri <richardokonicha@gmail.com>
2024-07-22 15:15:35 +0100

    initials

---
+++ hello-cargo/target/debug/deps/hello_cargo-f215f7c2041958e5.d
@@ -0,0 +1,5 @@
+/Users/riri/Documents/fugoku/rust_master/hello-cargo/target/debug/deps/hello_cargo-f215f7c2041958e5: src/main.rs
+
+/Users/riri/Documents/fugoku/rust_master/hello-cargo/target/debug/deps/hello_cargo-f215f7c2041958e5.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/Users/riri/Documents/fugoku/rust_master/hello-cargo/target/debug/hello-cargo: /Users/riri/Documents/fugoku/rust_master/hello-cargo/src/main.rs
